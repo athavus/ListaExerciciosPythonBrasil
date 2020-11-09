73e2dc22dbf5821007bb0f6c740e35897b34c763
Miguel Ryan Dantas de Freitas <73431866+athavus@users.noreply.github.com>
2020-11-09 13:38:06 -0300

    Add files via upload

---
+++ ExerciciosSequenciais/2num_int_1_real.py
@@ -0,0 +1,9 @@
+a = int(input('Digite um número inteiro: '))
+b = int(input('Digite um outro número inteiro: '))
+c = float(input('Digite um número real: '))
+la = (a * 2) * (b / 2)
+lb = (a * 3) + c
+lc = c ** 3
+print(f'O produto do dobro do primeiro número com metade do segundo número é igual a {la}')
+print(f'A soma do triplo do primeiro número com o terceiro é igual a {lb}')
+print(f'O terceiro número elevado ao cubo é igual a {lc}')
