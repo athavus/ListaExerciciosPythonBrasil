34d54cef268ce749aab6f1075e4793071c6d97ee
Miguel Ryan Dantas de Freitas <73431866+athavus@users.noreply.github.com>
2020-11-11 01:45:52 -0300

    Exercício 2 da lista de decisão

---
+++ ExercíciosDeDecisão/posi_nega.py
@@ -0,0 +1,7 @@
+n1 = float(input('Digite um número: '))
+if n1 > 0:
+    print(f'{n1} é um valor positivo!')
+elif n1 < 0:
+    print(f'{n1} é um valor negativo!')
+else:
+    print(f'{n1} é 0, ou seja, um valor nulo!')
