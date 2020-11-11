0d0778f99ef34ce23adffefaea9b1cbd56e704fb
Miguel Ryan Dantas de Freitas <73431866+athavus@users.noreply.github.com>
2020-11-11 01:51:49 -0300

    Ex 3 e 4

---
+++ ExercíciosDeDecisão/mascu_femi.py
@@ -0,0 +1,7 @@
+a = input('Digite uma letra: [F/M] ')
+if a in 'Mm':
+    print('Masculino!')
+elif a in 'Ff':
+    print('Feminino!')
+else:
+    print('Sexo inválido!')
---
+++ ExercíciosDeDecisão/vogal.py
@@ -0,0 +1,5 @@
+a = input('Digite uma letra: ').lower()
+if a in 'aeiou':
+    print('É uma vogal!')
+else:
+    print('É uma consoante!')
