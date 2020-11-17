a7ee321d7d25901b1dca4e6d6c0ad2ef69ca55ee
Miguel Ryan Dantas de Freitas <73431866+athavus@users.noreply.github.com>
2020-11-17 18:27:34 -0300

    Add files via upload

---
+++ ExercíciosDeDecisão/numero_e_dia_da_semana.py
@@ -0,0 +1,18 @@
+numero_da_semana = int(input('Digite o número correspondente a o dia da semana: '))
+
+if numero_da_semana == 1:
+    print('Domingo')
+elif numero_da_semana == 2:
+    print('Segunda')
+elif numero_da_semana == 3:
+    print('Terça')
+elif numero_da_semana == 4:
+    print('Quarta')
+elif numero_da_semana == 5:
+    print('Quinta')
+elif numero_da_semana == 6:
+    print('Sexta')    
+elif numero_da_semana == 7:
+    print('Sábado')
+else:
+    print('Valor inválido')
